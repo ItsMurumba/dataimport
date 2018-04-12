540de2d4e287f34860678ee02289f3a749d4124c
KelvinMurumba <kevmurumba@gmail.com>
2018-04-12 07:38:30 +0300

    Added mysql connector dependencies

--- src/main/java/com/ppm/dataimport/DataimportApplication.java
+++ src/main/java/com/ppm/dataimport/DataimportApplication.java
@@ -1,12 +1,21 @@
 package com.ppm.dataimport;
 
+import com.ppm.dataimport.resource.ExcelReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class DataimportApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DataimportApplication.class, args);
+
+//		// TODO code application logic here
+//		File excelFile = new File("/home/iita/Kelvin/JavaProjects/dataimport/uploads/UCSF/Oct17_Jan18_UCSF_ IPD.xlsx");
+//		ExcelReader er = new ExcelReader(excelFile);
+//		er.getRowAsListFromExcel();
 	}
 }
---
+++ src/main/java/com/ppm/dataimport/resource/ExcelReader.java
@@ -0,0 +1,121 @@
+package com.ppm.dataimport.resource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ppm.dataimport.repository.DataSetRepository;
+import com.ppm.dataimport.service.DataSets;
+import com.ppm.dataimport.service.OrganiationUnits;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.DataFormatter;
+
+import com.opencsv.CSVWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ExcelReader {
+    @Autowired
+    DataSets dataSets;
+    @Autowired
+    OrganiationUnits organiationUnits;
+
+    private final File file;
+    DataFormatter formatter = new DataFormatter();
+
+    public ExcelReader(File file) {
+        this.file = file;
+    }
+
+    //Convert the excel into a List
+    public void getRowAsListFromExcel() {
+
+        //Array List to store the excel data
+        List<String[]> csvList = new ArrayList<>();
+        FileInputStream fis;
+        Workbook workbook = null;
+        int maxDataCount = 0;
+        try {
+            String fileExtension = file.toString().substring(file.toString().indexOf("."));
+            OPCPackage oPCPackage = OPCPackage.open(file);
+            //use xssf for xlsx format else hssf for xls format
+            switch (fileExtension) {
+                case ".xlsx":
+                    workbook = new XSSFWorkbook(oPCPackage);
+                    break;
+                case ".xls":
+//                    workbook = new HSSFWorkbook(new POIFSFileSystem(fis));
+                    System.err.println("Wrong file type selected!");
+                    break;
+                default:
+                    System.err.println("Wrong file type selected!");
+                    break;
+            }
+
+            //get number of worksheets in the workbook
+            int numberOfSheets = workbook.getNumberOfSheets();
+
+
+            //iterating over each workbook sheet
+            for (int i = 0; i < numberOfSheets; i++) {
+                Sheet sheet = workbook.getSheetAt(i);
+                int numberOfRows = sheet.getLastRowNum();
+
+                for (int row = 4; row <= numberOfRows; row++) {
+                    Row currentRow = sheet.getRow(row);
+                    int numberOfCells = currentRow.getLastCellNum();
+
+                    String facility = currentRow.getCell(0, Row.CREATE_NULL_AS_BLANK).getStringCellValue();
+                    int mflcode = (int) currentRow.getCell(1, Row.CREATE_NULL_AS_BLANK).getNumericCellValue();
+                    int period = (int) currentRow.getCell(2, Row.CREATE_NULL_AS_BLANK).getNumericCellValue();
+
+                    String[] dataRows = new String[9];
+
+                    int dataElementsLength =dataSets.IPD().length;
+                    System.out.printf(String.valueOf(dataElementsLength));
+                    dataRows[6] = dataSets.IPD()[24][0];
+                    dataRows[8] = dataSets.IPD()[25][0];
+                    dataRows[4] = organiationUnits.getOrgUid(mflcode);
+                    for (int x = 3; x < dataElementsLength-2; x++) {
+                        for (int y = 0; y < 2; y++) {
+                            dataRows[0] = dataSets.IPD()[x][y];
+                            System.out.printf("%s  ", dataRows[0]);
+                        }
+                        int dataValue = (int) currentRow.getCell(x, Row.CREATE_NULL_AS_BLANK).getNumericCellValue();
+                        dataRows[2] = String.valueOf(period);
+                        dataRows[3] = facility;
+                        dataRows[7] = String.valueOf(dataValue);
+                        csvList.add(dataRows);
+                        System.out.printf("%d\t%s\t%d\t%s\t%s\t%d\t%s\n", period, facility, mflcode,dataRows[4], dataRows[6], dataValue, dataRows[8]);
+                        System.out.printf("\n");
+                    }
+
+                    System.out.printf(String.valueOf(organiationUnits.organizationalUnit()));
+//                    System.exit(0);
+                }
+            }
+            oPCPackage.close();
+            writeRowToCSVFile(csvList);
+        } catch (IOException | InvalidFormatException e) {
+        }
+    }
+
+    /*
+     * Write the rows into the CSV file
+	 */
+    private static void writeRowToCSVFile(List<String[]> cleanRows)
+            throws IOException {
+        File newFile = new File("/home/iita/Kelvin/JavaProjects/dataimport/output/oct17_jan18_ucsf_ipd_results_data.csv");
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter(newFile))) {
+            csvWriter.writeAll(cleanRows);
+        }
+    }
+}
